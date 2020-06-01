c54cd928a880fa8b9a51295e55169276db6a8295
Erik Ma <ericma19920728@gmail.com>
2020-06-01 16:53:18 +0800

    GO_IO

---
+++ GO/GO_IO/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+type ModuleAndTPEVersion struct {
+	Module     string `json:Module`
+	TPEVersion string `json:TPEVesion`
+}
+
+func main() {
+	var deviceInfo ModuleAndTPEVersion
+	// read file
+	b, _ := ioutil.ReadFile("/etc/pversion")
+	if string(b[len(b)-1:]) == "\n" {
+		b = b[:len(b)-1]
+	}
+	deviceInfo.Module = strings.Split(string(b), " ")[0]
+	deviceInfo.TPEVersion = strings.Split(string(b), " ")[1]
+	fmt.Println(deviceInfo)
+	fmt.Println(deviceInfo.Module)
+	fmt.Println(deviceInfo.TPEVersion)
+
+	// get pwd
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dir)
+
+	// write file
+	jsonOutPut, _ := json.MarshalIndent(deviceInfo, "", " ")
+	_ = ioutil.WriteFile(dir+"/version", jsonOutPut, 0644)
+
+	// read json file
+	var readDeviceInfo ModuleAndTPEVersion
+	jsonRead, _ := ioutil.ReadFile(dir + "/version")
+	_ = json.Unmarshal(jsonRead, &readDeviceInfo)
+	fmt.Println(readDeviceInfo)
+
+}
